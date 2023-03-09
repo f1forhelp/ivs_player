12a3f7ed7a0e2e4694a5e6304807e12c5cea06b2
Utkarsh Sharma <rickysharma1111@gmail.com>
2023-03-09 12:02:01 +0530

    Different player screen

--- example/lib/basic_player_screen.dart
+++ example/lib/basic_player_screen.dart
@@ -46,34 +46,34 @@ class _PlayerScreenState extends State<PlayerScreen> {
           BaseIvsPlayer(
             onPlatformViewCreated: (p0) {
               IvsPlayerApi().load(
-                  "https://takapp-media-cdn.s3.ap-south-1.amazonaws.com/production/vod_22_Nov_2022_vdt-1/video.m3u8");
+                  "https://www.hlsplayer.net/#type=m3u8&src=https%3A%2F%2Fdemo.unified-streaming.com%2Fk8s%2Ffeatures%2Fstable%2Fvideo%2Ftears-of-steel%2Ftears-of-steel.mp4%2F.m3u8");
               IvsPlayerApi().play();
             },
             ivsPlayerController: _ivsPlayerController,
           ),
-          Row(
-            children: [
-              CustomTextButton(
-                label: "Load",
-                ontap: () {
-                  IvsPlayerApi().load(
-                      "https://takapp-media-cdn.s3.ap-south-1.amazonaws.com/production/vod_22_Nov_2022_vdt-1/video.m3u8");
-                },
-              ),
-              CustomTextButton(
-                label: "Play",
-                ontap: () {
-                  IvsPlayerApi().play();
-                },
-              ),
-              CustomTextButton(
-                label: "Pause",
-                ontap: () {
-                  IvsPlayerApi().pause();
-                },
-              ),
-            ],
-          ),
+          // Row(
+          //   children: [
+          //     CustomTextButton(
+          //       label: "Load",
+          //       ontap: () {
+          //         IvsPlayerApi().load(
+          //             "https://takapp-media-cdn.s3.ap-south-1.amazonaws.com/production/vod_22_Nov_2022_vdt-1/video.m3u8");
+          //       },
+          //     ),
+          //     CustomTextButton(
+          //       label: "Play",
+          //       ontap: () {
+          //         IvsPlayerApi().play();
+          //       },
+          //     ),
+          //     CustomTextButton(
+          //       label: "Pause",
+          //       ontap: () {
+          //         IvsPlayerApi().pause();
+          //       },
+          //     ),
+          //   ],
+          // ),
         ],
       ),
     );
